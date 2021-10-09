f7ffd67083268455094e82476469000b9a581e2f
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-10-09 18:34:56 -0500

    ⭐️ Added sortable, open snippet in app function, and more icons

--- settings/settingsData.ts
+++ settings/settingsData.ts
@@ -1,7 +1,21 @@
+interface Applications {
+  name: string;
+  code: string;
+  arguments: string;
+}
+
 export interface MySnippetsSettings {
   aestheticStyle: boolean;
+  applications: Applications[];
 }
 
 export const DEFAULT_SETTINGS: MySnippetsSettings = {
   aestheticStyle: false,
+  applications: [
+    {
+      name: "Sublime",
+      code: "Sublime Text",
+      arguments: "",
+    },
+  ],
 };
