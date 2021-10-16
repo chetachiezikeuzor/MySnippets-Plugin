398f5d7e307f0cbd90b16196a3af8429d117f58e
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-10-16 15:47:59 -0500

    ✍🏽 Changes to schema

--- src/settings/settingsData.ts
+++ src/settings/settingsData.ts
@@ -1,19 +1,11 @@
-interface Applications {
-  name: string;
-  code: string;
-  arguments: string;
-}
-
 export const SNIPPET_VIEW_SIDE = ["right", "left"];
 
 export interface MySnippetsSettings {
   aestheticStyle: boolean;
-  applications: Applications[];
   snippetViewPosition: string;
 }
 
 export const DEFAULT_SETTINGS: MySnippetsSettings = {
   aestheticStyle: false,
-  applications: [],
   snippetViewPosition: "left",
 };
